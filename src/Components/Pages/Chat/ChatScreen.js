b533cdcf8c7394c66673035509bb6148ab3b8c0d
tpeacefill <pamenuveve.ac@st.vvu.edu.gh>
2024-04-03 03:12:02 +0000

    Emoji picker set to post texr

--- src/Components/Pages/Chat/ChatScreen.js
+++ src/Components/Pages/Chat/ChatScreen.js
@@ -13,6 +13,7 @@ import {
   serverTimestamp,
 } from "firebase/firestore";
 import { signOut } from "firebase/auth";
+import { onSnapshot } from "firebase/firestore";
 import "./Chat.css";
 import Sidepane from "../../App-Components/Sidepane";
 import Menubar from "../../App-Components/Menubar";
@@ -48,17 +49,21 @@ function ChatScreen() {
         where("senderId", "in", [auth.currentUser.uid, selectedChat.userId]),
         where("receiverId", "in", [auth.currentUser.uid, selectedChat.userId])
       );
-      const querySnapshot = await getDocs(q);
 
-      const fetchedMessages = [];
-      querySnapshot.forEach((doc) => {
-        const messageData = doc.data();
-        fetchedMessages.push(messageData);
+      // Subscribe to real-time updates for the messages query
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        const fetchedMessages = [];
+        snapshot.forEach((doc) => {
+          const messageData = doc.data();
+          fetchedMessages.push(messageData);
+        });
+
+        fetchedMessages.sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1));
+        setMessages(fetchedMessages);
       });
 
-      fetchedMessages.sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1));
-
-      setMessages(fetchedMessages);
+      // Return the unsubscribe function to clean up the listener
+      return unsubscribe;
     };
 
     fetchMessages();
@@ -186,6 +191,11 @@ function ChatScreen() {
     }
   }, []);
 
+  const handleEmojiSelect = (emoji) => {
+    // Insert the selected emoji into the current postText value
+    setPostText((prevPostText) => prevPostText + emoji.emoji);
+  };
+
   const logout = async () => {
     try {
       await signOut(auth);
@@ -293,7 +303,8 @@ function ChatScreen() {
                       viewBox="0 0 24 24"
                       className="emoji-trigger"
                     >
-                      <g fill="none">
+                      {/* Add onMouseDown event to prevent focus loss when clicking on the emoji button */}
+                      <g fill="none" onMouseDown={(e) => e.preventDefault()}>
                         <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
                         <path
                           fill="currentColor"
